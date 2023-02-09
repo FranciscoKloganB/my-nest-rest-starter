40dd3e66c0febba5214ef6a6125e0c66b230c6fb
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2023-02-09 09:02:26 +0000

    chore: bump dependencies - 20230209

--- jest.setup.ts
+++ jest.setup.ts
@@ -3,9 +3,11 @@
  *  jest.spyOn(bcrypt, 'hash').mockReturnValue("hello-world")
  *  jest.spyOn(bcrypt, 'compare').mockReturnValue(true)
  */
-jest.mock('bcrypt', () => ({
+jest.mock("bcrypt", () => ({
   __esModule: true,
-  ...jest.requireActual('bcrypt'),
-  hash: jest.fn(),
+  ...jest.requireActual("bcrypt"),
   compare: jest.fn(),
-}));
+  hash: jest.fn(),
+}))
+
+export {}
